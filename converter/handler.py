508f20fa080d87c3da664ba4820c7b4c70c3cf58
Katerina <ekaterina.frolova40@gmail.com>
2021-02-15 10:54:09 +0300

    Added formating response  data

--- converter/handler.py
+++ converter/handler.py
@@ -1,5 +1,6 @@
 """Module implementing http handler """
 
+import json
 from http import server
 from urllib.parse import urlparse
 
@@ -7,14 +8,14 @@ from logger import get_custom_logger
 from .currency_converter import ConverterClient
 
 
-logger = get_custom_logger(__name__)
-
-
 class ConverterHTTPHandler(server.SimpleHTTPRequestHandler):
     converter = ConverterClient()
 
     def do_GET(self):
         currencies_data = self.converter.get_currencies_dict(self.path)
-        self.send_response(200)
+        code = 400 if isinstance(currencies_data, str) else 200
+        data = currencies_data if isinstance(currencies_data, str) else json.dumps(currencies_data)
+        self.send_response(code)
         self.send_header("Content-type", "application/json")
         self.end_headers()
+        self.wfile.write(data.encode())
